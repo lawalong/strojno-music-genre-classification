9c6354cb6ea60c2970200f1769596ffbfb85204c
tomislav.coh <tomislav.coh@ac5171a8-ed51-11de-a70e-4b51a9783938>
2009-12-20 10:32:03 +0000

    commitam ovo lukino prvo

---
+++ trunk/src/hr/fer/su/mgc/audio/AudioFile.java
@@ -0,0 +1,324 @@
+package hr.fer.su.mgc.audio;
+
+import hr.fer.su.mgc.swing.MGCSwingMain;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Map;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JLabel;
+
+import org.tritonus.share.sampled.TAudioFormat;
+import org.tritonus.share.sampled.file.TAudioFileFormat;
+
+public class AudioFile {
+	
+	private File audioFile;
+	
+	private AudioInputStream inputStream;
+	private AudioInputStream decodedInputStream;
+	
+	private AudioFileFormat baseFileFormat;
+	private AudioFormat baseFormat;
+	
+	private AudioFormat decodedFormat;
+	
+	private SourceDataLine line;
+	
+	/**
+	 * State variable.<br>
+	 * 0 := stopped
+	 * 1 := paused
+	 * 2 := running
+	 */
+	public Integer state;
+	
+	private PlaybackThread playback;
+	
+	private SliderThread sliderThread;
+
+	@SuppressWarnings("unchecked")
+	public AudioFile(String filePath, MGCSwingMain mainRef)
+			throws UnsupportedAudioFileException, IOException {
+		audioFile = new File(filePath);
+
+		state = new Integer(0);
+
+		// Checking for errors...
+		inputStream = AudioSystem.getAudioInputStream(audioFile);
+
+		baseFileFormat = AudioSystem.getAudioFileFormat(audioFile);
+		baseFormat = baseFileFormat.getFormat();
+
+		decodedFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
+				baseFormat.getSampleRate(), 16, baseFormat.getChannels(),
+				baseFormat.getChannels() * 2, baseFormat.getSampleRate(), false);
+		inputStream.close();
+
+		// TAudioFileFormat properties
+		if (baseFileFormat instanceof TAudioFileFormat) {
+			Map properties = ((TAudioFileFormat) baseFileFormat).properties();
+			int length = Integer.valueOf(properties.get("duration").toString());
+
+			// Setting maximum in miliseconds...
+			mainRef.playerSlider.setMaximum(length / 1000);
+
+			String title = properties.get("title").toString();
+			String author = properties.get("author").toString();
+			String genre = properties.get("mp3.id3tag.genre").toString();
+			if (title == null)
+				title = "Unknown";
+			if (author == null)
+				author = "Unknown";
+			if (genre == null)
+				genre = "Unknown";
+
+			mainRef.tagLabel.setText("<html><font size=+2><b>" + title
+					+ "</b></font> by " + author + " [" + genre + "]</html>");
+
+		} else if (baseFormat instanceof TAudioFormat) {
+			Map properties = ((TAudioFormat) baseFormat).properties();
+			int length = Integer.valueOf(properties.get("duration").toString());
+			// Setting maximum in miliseconds...
+			mainRef.playerSlider.setMaximum(length / 1000);
+		
+		} else {
+
+			// Setting maximum in miliseconds...
+			mainRef.playerSlider.setMaximum((int)
+					(audioFile.length()/(baseFormat.getFrameSize()*baseFormat.getFrameRate())*1000));
+		}
+		
+		mainRef.playerSlider.setMajorTickSpacing(mainRef.playerSlider.getMaximum()/8);
+		mainRef.playerSlider.setMinorTickSpacing(mainRef.playerSlider.getMajorTickSpacing()/5);
+
+		Hashtable<Integer, JLabel> labelTable = new Hashtable<Integer, JLabel>();
+		for (int i = mainRef.playerSlider.getMinimum(); i <= mainRef.playerSlider
+				.getMaximum(); i += mainRef.playerSlider.getMajorTickSpacing()) {
+
+			int secs = i / 1000, mins = 0;
+			while (secs >= 60) {
+				mins++;
+				secs -= 60;
+			}
+
+			labelTable.put(i, new JLabel(String.valueOf(mins) + ":"
+					+ ((secs < 10) ? "0" + secs : secs)));
+		}
+		mainRef.playerSlider.setLabelTable(labelTable);
+
+	}
+	
+	private void playInit(MGCSwingMain mainRef) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
+		inputStream = AudioSystem.getAudioInputStream(audioFile);
+		
+		baseFileFormat = AudioSystem.getAudioFileFormat(audioFile);
+		baseFormat = baseFileFormat.getFormat();
+		
+		decodedFormat = new AudioFormat(
+				AudioFormat.Encoding.PCM_SIGNED,
+				baseFormat.getSampleRate(), 16, baseFormat.getChannels(),
+				baseFormat.getChannels() * 2, baseFormat.getSampleRate(),
+				false);
+		decodedInputStream = AudioSystem.getAudioInputStream(decodedFormat, inputStream);
+		
+		line = getLine(decodedFormat);
+		
+		playback = new PlaybackThread();
+		sliderThread = new SliderThread(mainRef);
+	}
+
+
+	private SourceDataLine getLine(AudioFormat audioFormat)
+			throws LineUnavailableException {
+		SourceDataLine res = null;
+		DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+		res = (SourceDataLine) AudioSystem.getLine(info);
+		res.open(audioFormat);
+		return res;
+	}
+	
+	public void play(MGCSwingMain mainRef) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+		
+		// Wait for last stop request to finish...
+		while(state == -1) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException Ignorable) { }
+		}
+		
+		synchronized (state) {
+			switch(state) {
+			case 0:
+				playInit(mainRef);
+				line.start();
+				state = 2;
+				sliderThread.start();
+				playback.start();
+				break;
+			case 1:
+				line.start();
+				state = 2;
+				synchronized(playback) {
+					playback.notify();
+				}
+				synchronized(sliderThread) {
+					sliderThread.notify();
+				}
+				
+				break;
+			case 2: // Restart playback...
+				stop();
+				play(mainRef);
+				break;
+			}
+		}
+	}
+	
+	public void pause() {
+		synchronized (state) {
+			if(state == 2) {
+				line.stop();
+				state = 1;
+			}
+		}
+	}
+	
+	public void seek(long bytesToSkip) {		// TODO
+		try {
+			line.stop();
+			line.flush();
+			decodedInputStream.reset();
+			decodedInputStream.skip(bytesToSkip);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	public void stop() {
+		synchronized (state) {
+			int tmpState = state;
+			state = 0;
+			if(tmpState == 1) {
+				synchronized(playback) {
+					playback.notify();
+				}
+				synchronized(sliderThread) {
+					sliderThread.notify();
+				}
+			} else if(tmpState == 2) {
+				line.stop();
+				close();
+			}
+		}
+	}
+	
+	
+	private void close() {
+		try {
+			state = 0;
+			line.close();
+			inputStream.close();
+		} catch (Exception Ignorable) { }
+	}
+
+
+	class PlaybackThread extends Thread {
+		
+		private byte[] data;
+
+		public PlaybackThread() {
+			data = new byte[8192];
+		}
+
+		@Override
+		public void run() {
+			int nBytesRead = 0;
+			
+			try {
+				while (nBytesRead != -1) {
+					switch(state) {
+					case 0: return;
+					case 1:
+						while(state == 1) {
+							try {
+								synchronized (this) {
+									this.wait();
+								}
+							} catch (InterruptedException ex) {
+								continue;
+							}
+						}
+						if(state == 0) return;
+						break;
+					}
+
+					
+					nBytesRead = decodedInputStream.read(data, 0, data.length);
+
+					if (nBytesRead != -1) {
+						line.write(data, 0, nBytesRead);
+					}
+					
+				}
+				line.drain();
+			
+			} catch (IOException Ignorable) {}
+			close();
+		}
+	}
+	
+	class SliderThread extends Thread {
+		private MGCSwingMain mainRef;
+
+		public SliderThread(MGCSwingMain mainRef) {
+			this.mainRef = mainRef;
+		}
+
+		@Override
+		public void run() {
+			while(true) {
+				switch(state) {
+				case 0: 
+					mainRef.playerSlider.setValue(0);
+					return;
+				case 1:
+					while(state == 1) {
+						try {
+							synchronized (this) {
+								this.wait();
+							}
+						} catch (InterruptedException ex) {
+							continue;
+						}
+					}
+					if(state == 0) {
+						mainRef.playerSlider.setValue(0);
+						return;
+					}
+					break;
+				}
+//				mainRef.timeLabel.setText(text)
+				mainRef.playerSlider.setValue((int)(line.getMicrosecondPosition()/1000));
+				try {
+					Thread.sleep(80);
+				} catch (InterruptedException Ignorable) { }
+				
+			}
+		}
+	}
+
+
+}
---
+++ trunk/src/hr/fer/su/mgc/swing/MGCSwingMain.java
@@ -0,0 +1,358 @@
+package hr.fer.su.mgc.swing;
+
+import hr.fer.su.mgc.audio.AudioFile;
+import hr.fer.su.mgc.swing.image.ImageUtils;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.GraphicsEnvironment;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.plaf.FontUIResource;
+
+import com.nilo.plaf.nimrod.NimRODLookAndFeel;
+
+public class MGCSwingMain extends JFrame {
+	private static final long serialVersionUID = -489245112812832880L;
+	
+	/** 
+	 * Main frame reference..
+	 */
+	public static Frame frameRef;
+	
+	/**
+	 * Map that contains used actions.
+	 */
+	private Map<String, Action> actions;
+	
+	/**
+	 * Audio file chooser reference.
+	 */
+	private JFileChooser fileChooser;
+	
+	private AudioFile audioFile;
+	
+	public JLabel tagLabel;
+	public JLabel timeLabel;
+	public JTextField inputField;
+	public JSlider playerSlider;
+
+	
+	
+	public MGCSwingMain() {
+		
+		frameRef = this;
+		
+		NimRODLookAndFeel NimRODLF = new NimRODLookAndFeel();
+
+		// NimRODLookAndFeel.setCurrentTheme(SSMUMain.makeNimrodTheme());
+		
+		try {
+			UIManager.setLookAndFeel(NimRODLF);
+		} catch (Exception Ignorable) { }
+		
+//		try {
+//			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//		} catch (Exception Ignorable) { }
+		
+		@SuppressWarnings("unused")
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		setLocation(200, 200);
+		setSize(800, 300);
+		setMaximizedBounds(GraphicsEnvironment.
+				getLocalGraphicsEnvironment().getMaximumWindowBounds());
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// Do something...
+			}
+		});
+		setTitle("Music Genre Classifier");
+		
+		actions = createActions();
+		ImageUtils.loadIconsAndComponents();
+		
+		initGUI();
+	}
+	
+	/**
+	 * GUI Initialization...
+	 */
+	@SuppressWarnings("serial")
+	private void initGUI() {
+		final MGCSwingMain mainRef = this;
+		
+		tagLabel = new JLabel();
+		timeLabel = new JLabel();
+		inputField = new JTextField();
+		playerSlider = new JSlider(JSlider.HORIZONTAL, 0, 0, 0);
+		
+		createMenus();
+		
+		setLayout(new BorderLayout());
+		
+		JPanel playerPanel = new JPanel(new BorderLayout());
+		playerPanel.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
+		add(playerPanel, BorderLayout.NORTH);
+		
+		JPanel playerPanelInner = new JPanel();
+		playerPanel.add(playerPanelInner, BorderLayout.CENTER);
+		playerPanelInner.setLayout(new BoxLayout(playerPanelInner, BoxLayout.Y_AXIS));
+		playerPanelInner.setBorder(BorderFactory.createTitledBorder("Player"));
+		
+		
+		
+		JPanel browsePanel = new JPanel(new BorderLayout(8, 0));
+		browsePanel.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
+		browsePanel.add(inputField, BorderLayout.CENTER);
+		
+		Action browseAction = new AbstractAction("Browse") {
+			public void actionPerformed(ActionEvent event) {
+				int status = fileChooser.showOpenDialog(mainRef);
+
+				if (status == JFileChooser.APPROVE_OPTION) {
+					File selectedFile = fileChooser.getSelectedFile();
+					try {
+						if(audioFile != null) {
+							tagLabel.setText("");
+							timeLabel.setText("");
+							audioFile.stop();
+						}
+						audioFile = new AudioFile(
+								selectedFile.getAbsolutePath(), mainRef);
+						inputField.setText(selectedFile.getName());
+					} catch (UnsupportedAudioFileException e) {
+						JOptionPane.showMessageDialog(mainRef, "Unsupported Audio File!", 
+								"Audio loading error", JOptionPane.ERROR_MESSAGE);
+					} catch (IOException e) {
+						JOptionPane.showMessageDialog(mainRef, "IO Error: " + e.getLocalizedMessage(), 
+								"Audio loading error", JOptionPane.ERROR_MESSAGE);
+					}
+
+				}
+			}
+		};
+		browseAction.putValue(Action.SHORT_DESCRIPTION, "Browse for audio file...");
+		
+		JButton browseButton = new JButton(browseAction);
+		browsePanel.add(browseButton, BorderLayout.EAST);
+		playerPanelInner.add(browsePanel);
+		
+		
+		JPanel tagsPanel = new JPanel(new BorderLayout());
+		tagsPanel.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
+		tagsPanel.add(tagLabel, BorderLayout.WEST);
+		tagsPanel.add(timeLabel, BorderLayout.EAST);
+		playerPanelInner.add(tagsPanel);
+		
+		JPanel sliderPanel = new JPanel(new BorderLayout());
+		sliderPanel.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
+		playerSlider.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		//Turn on labels at major tick marks.
+		playerSlider.setMajorTickSpacing(10000);
+		playerSlider.setMinorTickSpacing(2000);
+		playerSlider.setPaintTicks(true);
+		playerSlider.setPaintLabels(true);
+		
+		Hashtable<Integer,JLabel> labelTable = new Hashtable<Integer,JLabel>();
+		for(int i = playerSlider.getMinimum(); 
+			i <= playerSlider.getMaximum(); i += playerSlider.getMajorTickSpacing()) {
+			
+			int secs = i/1000, mins = 0;
+			while(secs >= 60) { mins++; secs -= 60; }
+			
+			labelTable.put(i, new JLabel(String.valueOf(mins) + 
+					":" + ((secs < 10) ? "0" + secs : secs)));
+		}
+		playerSlider.setLabelTable(labelTable);
+
+
+		sliderPanel.add(playerSlider, BorderLayout.CENTER);
+		playerPanelInner.add(sliderPanel);
+		
+		
+		JPanel buttonsPanel = new JPanel(new BorderLayout());
+		buttonsPanel.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
+		final JPanel buttonsPanelLeft = new JPanel(new GridLayout(1, 3, 8, 0));
+		
+		
+		Action playerPlay = new AbstractAction("Play") {
+			public void actionPerformed(ActionEvent event) {
+				if(audioFile != null) {
+					try {
+						audioFile.play(mainRef);
+					} catch (UnsupportedAudioFileException e) {
+						JOptionPane.showMessageDialog(mainRef, "Unsupported Audio File!", 
+								"Audio loading error", JOptionPane.ERROR_MESSAGE);
+					} catch (IOException e) {
+						JOptionPane.showMessageDialog(mainRef, "IO Error: " + e.getLocalizedMessage(), 
+								"Audio playback error", JOptionPane.ERROR_MESSAGE);
+					} catch (LineUnavailableException e) {
+						JOptionPane.showMessageDialog(mainRef, "No line is available!", 
+								"Audio playback error", JOptionPane.ERROR_MESSAGE);
+					}
+				}
+			}
+		};
+		playerPlay.putValue(Action.SHORT_DESCRIPTION, "Start playback");
+		JButton playButton = new JButton(playerPlay);
+		buttonsPanelLeft.add(playButton);
+
+		
+		Action playerPause = new AbstractAction("Pause") {
+			public void actionPerformed(ActionEvent event) {
+				if(audioFile != null) {
+					audioFile.pause();
+				}
+			}
+		};
+		playerPause.putValue(Action.SHORT_DESCRIPTION, "Pause playback");
+		JButton pauseButton = new JButton(playerPause);
+		buttonsPanelLeft.add(pauseButton);
+		
+		
+		Action playerStop = new AbstractAction("Stop") {
+			public void actionPerformed(ActionEvent event) {
+				if(audioFile != null) {
+					audioFile.stop();
+				}
+			}
+		};
+		playerStop.putValue(Action.SHORT_DESCRIPTION, "Stop playback");
+		JButton stopButton = new JButton(playerStop);
+		buttonsPanelLeft.add(stopButton);
+		
+		buttonsPanel.add(buttonsPanelLeft, BorderLayout.WEST);
+		
+		playerPanelInner.add(buttonsPanel);
+		
+	}
+	
+	/**
+	 * Menus Creation.
+	 */
+	private void createMenus() {
+		
+		JMenuBar menuBar = new JMenuBar();
+		
+		JMenu menuFile = new JMenu("File");
+		
+		menuFile.addSeparator();
+		
+		JMenuItem exitMenuItem = new JMenuItem(actions.get("Exit"));
+		// exitMenuItem.setIcon(ImageUtils.getIcon("exitSim"));
+		menuFile.add(exitMenuItem);
+		
+		menuBar.add(menuFile);
+		
+		JMenu menuHelp = new JMenu("Help");
+		
+		JMenuItem aboutMenuItem = new JMenuItem(actions.get("helpAbout"));
+		menuHelp.add(aboutMenuItem);
+		
+		menuBar.add(menuHelp);
+
+		setJMenuBar(menuBar);
+	}
+	
+	
+	/**
+	 * Creation of actions.
+	 */
+	@SuppressWarnings("serial")
+	private Map<String, Action> createActions() {
+		actions = new HashMap<String, Action>();
+		
+		fileChooser = new JFileChooser(".");
+		
+		
+		// Menus actions...
+
+		Action fileExit = new AbstractAction("Exit") {
+			public void actionPerformed(ActionEvent event) {
+				System.exit(0);
+			}
+		};
+		fileExit.putValue(Action.SHORT_DESCRIPTION, "Exit application");
+		fileExit.putValue(Action.MNEMONIC_KEY, new Integer(KeyEvent.VK_X));
+		actions.put("Exit", fileExit);
+		
+		Action helpAbout = new AbstractAction("About MGC") {
+			public void actionPerformed(ActionEvent event) {
+				// TODO
+			}
+		};
+		helpAbout.putValue(Action.SHORT_DESCRIPTION, "About Music Genre Classifier");
+		helpAbout.putValue(Action.MNEMONIC_KEY, new Integer(KeyEvent.VK_A));
+		actions.put("helpAbout", helpAbout);
+		
+		return actions;
+	}
+
+	/**
+	 * Sets the default font for all Swing components.<br>
+	 * example:
+	 * setUIFont (new FontUIResource("Serif", Font.ITALIC, 12));
+	 * @param f desired font
+	 */
+	public static void setUIFont(FontUIResource f) {
+		Enumeration<Object> keys = UIManager.getDefaults().keys();
+		while (keys.hasMoreElements()) {
+			Object key = keys.nextElement();
+			Object value = UIManager.get(key);
+			if (value instanceof FontUIResource)
+				UIManager.put(key, f);
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new MGCSwingMain().setVisible(true);
+				// mainFrame.setExtendedState(mainFrame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+			}
+		});
+	}
+
+}

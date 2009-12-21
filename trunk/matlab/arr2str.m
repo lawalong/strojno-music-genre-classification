c788a6b6001f0cdd605a8e2b6732b761989ee63f
tomislav.coh <tomislav.coh@ac5171a8-ed51-11de-a70e-4b51a9783938>
2009-12-21 17:38:20 +0000

    male promjene oko matlab koda

--- trunk/matlab/arr2str.m
+++
@@ -1,7 +0,0 @@
-function str = arr2str( arr )
-%ARR2STR Summary of this function goes here
-%   Detailed explanation goes here
-    str = sprintf('%f', arr(1));
-    for i=2:length(arr); str = strcat(str, sprintf(', %f', arr(i))); end;
-end
-
